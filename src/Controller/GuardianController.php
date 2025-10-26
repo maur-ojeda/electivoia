d4592d62a181595095dbb080b4f3c28327a5573c
Mauricio Ojeda <mauricioojeda@MacBook-Air-de-Mauricio.local>
2025-10-26 17:44:48 -0300

    done

--- src/Controller/GuardianController.php
+++ src/Controller/GuardianController.php
@@ -1,8 +1,8 @@
 <?php
 
-
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,13 +13,20 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class GuardianController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
-    public function dashboard(): Response
+    public function dashboard(EntityManagerInterface $em): Response
     {
         $guardian = $this->getUser();
         $pupils = $guardian->getGuardianStudents();
 
+        foreach ($pupils as $pupil) {
+            $enrollments = $em->getRepository(\App\Entity\Enrollment::class)
+                ->findBy(['student' => $pupil]);
+            $pupilEnrollments[$pupil->getId()] = $enrollments;
+        }
+
         return $this->render('guardian/dashboard.html.twig', [
             'pupils' => $pupils,
+            'pupilEnrollments' => $pupilEnrollments, // ← Pasar los datos separadamente
         ]);
     }
 }
